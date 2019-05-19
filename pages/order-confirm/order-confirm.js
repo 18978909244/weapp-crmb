93d305d5c1b73a1b2dcd898d34c6db89e08c4c1b
michaelsugar <40854@qq.com>
2019-05-19 21:36:20 +0800

    n

--- pages/order-confirm/order-confirm.js
+++ pages/order-confirm/order-confirm.js
@@ -38,7 +38,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    options.id = options.id ||'94,90'
+    // options.id = options.id ||'94,90'
     console.log(options.id)
     app.setUserInfo();
     var that = this;
